940075ab01f58b56f7086ba4b88bebdb574c6be3
gclm <gclmit@163.com>
2022-10-28 16:01:36 +0800

    fix: 完善字节系规则

---
+++ crack/Block100SVIPCrack.js
@@ -0,0 +1,15 @@
+/*
+脚本功能：拦截100解锁超级会员
+软件版本：3.0.2【最高支持版本】
+下载地址：http://t.cn/A6MLFAJf
+脚本作者：Hausd0rff
+更新时间：2022-02-15
+*******************************
+*/
+
+
+var body = $response.body;
+body = "lvCQG8cCxqficLk+LttK+L2YRSLM39a3jj+Mfoba4wNaV8x54RYUAWrJvcwwFZemFEWG5xK2FlI8k2hlHsSW7uaAeW45GPxSPt1uvVwPI5zLiMJuHvGycPsqS+ozuIELZnJYBJk2unXdxQCbEof4Rmgo3m7O+4ByRFtdbPMM/COhtxmSVeHXnNcSfVlkALSnTyKl0xORwbczE2UdkNg/uioD0AtxIrGJ4jaUVNVKvJsibH0dOKL/bljPSrG31RQt";
+$done({ 
+    body 
+});
